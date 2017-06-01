27e6392d8910423e9d6f25126d59f80dca34057a
Keks <keks@htmlacademy.ru>
2017-06-01 12:04:13 +0300

    :kissing_cat: Добавляет сборку sass в gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,16 @@
 "use strict";
 
 var gulp = require("gulp");
-var less = require("gulp-less");
+var sass = require("gulp-sass");
 var plumber = require("gulp-plumber");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
 var server = require("browser-sync").create();
 
 gulp.task("style", function() {
-  gulp.src("less/style.less")
+  gulp.src("sass/style.scss")
     .pipe(plumber())
-    .pipe(less())
+    .pipe(sass())
     .pipe(postcss([
       autoprefixer({browsers: [
         "last 2 versions"
@@ -29,6 +29,6 @@ gulp.task("serve", ["style"], function() {
     ui: false
   });
 
-  gulp.watch("less/**/*.less", ["style"]);
+  gulp.watch("sass/**/*.{scss,sass}", ["style"]);
   gulp.watch("*.html").on("change", server.reload);
 });
