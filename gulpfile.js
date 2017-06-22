91a230fac178aa61e697425716320f6676db94ef
sheffcer <a.sheffcer@gmail.com>
2017-06-22 14:47:31 +0300

    добавил минификацию CSS

--- gulpfile.js
+++ gulpfile.js
@@ -74,9 +74,9 @@ gulp.task("style", function() {
       })
 
     ]))
-    // .pipe(gulp.dest("build/css"))
-    // .pipe(minify())
-    // .pipe(rename("style.min.css"))
+    .pipe(gulp.dest("build/css"))
+    .pipe(minify())
+    .pipe(rename("style.min.css"))
     .pipe(gulp.dest("build/css"))
     .pipe(server.stream());
 });
